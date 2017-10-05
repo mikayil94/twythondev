4c91f8f5010c08c04ad62d7c4e406eff11fa5d7c
Mikayil Ahadli <mikayilahadli@MacBook-Pro.local>
2017-10-06 01:33:12 +0400

    home deving

--- PycharmProjects/twitter/my_twitter.py
+++ PycharmProjects/twitter/my_twitter.py
@@ -1,29 +1,9 @@
-##from twython import Twython
-##
+from twython import *
+
 APP_KEY='yPWZw2YvawVuL0KQXPxbliv1b'
 APP_SECRET='gbQO12W2tg5wIKZP7hNhqEQaDCmhmbzrZ2MT12TPcy7uKGIilo'
 OAUTH_TOKEN='557947438-vTev6xeaCcPZXOAXXEwWIJRqVkshab2DTLEQH1qz'
 OAUTH_TOKEN_SECRET='aAUOING0pkL4Ew79fGArvimSfsYie0sf8KpczNxak22PO'
-##twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
-##
-##for status in twitter.search(q='"Mikayil"')["statuses"]:
-##    user = status["user"]["screen_name"].encode('utf-8')
-##    text = status["text"].encode('utf-8')
-##    print user, ":", text
-##    print
-
-##
-##from twython import *
-##twitter = Twython()
-### First, let's grab a user's timeline. Use the
-### 'screen_name' parameter with a Twitter user name.
-##user_timeline = twitter.getUserTimeline(screen_name="pythoncentral")
-##
-### And print the tweets for that user.
-##for tweet in user_timeline:
-##    print(tweet['text'])
-
-from twython import TwythonStreamer
 
 # appending data to a global variable is pretty poor form
 # but it makes the example much simpler
@@ -53,11 +33,27 @@ class MyStreamer(TwythonStreamer):
         print status_code, data
         self.disconnect()
 
-def call_twitter_streaming_api():
-    stream = MyStreamer(APP_KEY, APP_SECRET,
-                        OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+stream = MyStreamer(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+stream.statuses.filter(track=['mikayil',
+                              'azerbaijan'])
 
-    # starts consuming public statuses that contain the keyword 'data'
-    stream.statuses.filter(track='data science')
 
-call_twitter_streaming_api()
+
+##twitter = Twython(APP_KEY, APP_SECRET, OAUTH_TOKEN, OAUTH_TOKEN_SECRET)
+##
+##for status in twitter.search(q='"Mikayil"')["statuses"]:
+##    user = status["user"]["screen_name"].encode('utf-8')
+##    text = status["text"].encode('utf-8')
+##    print user, ":", text
+##    print
+
+##
+##from twython import *
+##twitter = Twython()
+### First, let's grab a user's timeline. Use the
+### 'screen_name' parameter with a Twitter user name.
+##user_timeline = twitter.getUserTimeline(screen_name="pythoncentral")
+##
+### And print the tweets for that user.
+##for tweet in user_timeline:
+##    print(tweet['text'])
